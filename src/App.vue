fcfe8c7a8f8059688665f9383fd76b7d5785fc56
bobbykim89 <bobby.sihun.kim@gmail.com>
2022-10-12 20:12:46 -0700

    add actions

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,11 @@
       <h1>Pinia Tasks</h1>
     </header>
 
+    <!-- new task form -->
+    <div class="new-task-form">
+      <TaskForm />
+    </div>
+
     <!-- filter -->
     <nav class="filter">
       <button @click="filter = 'all'">All tasks</button>
@@ -33,6 +38,7 @@
 import { ref } from 'vue'
 import { useTaskStore } from './stores/TaskStore'
 import TaskDetails from './components/TaskDetails.vue'
+import TaskForm from './components/TaskForm.vue'
 
 const TaskStore = useTaskStore()
 
