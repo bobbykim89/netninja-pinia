de94d3c067bd1afbe69854ead70c5d2ff757cf29
bobbykim89 <bobby.sihun.kim@gmail.com>
2022-10-17 22:24:05 -0700

    add reset button and destructure states and getters using storeToRefs

--- src/App.vue
+++ src/App.vue
@@ -18,22 +18,24 @@
     </nav>
 
     <!-- loading -->
-    <div class="loading" v-if="TaskStore.loading">Loading tasks...</div>
+    <div class="loading" v-if="loading">Loading tasks...</div>
 
     <!-- task list -->
     <div class="task-list" v-if="filter === 'all'">
-      <p>You have {{ TaskStore.totalCount }} tasks left to do</p>
-      <div v-for="task in TaskStore.tasks">
+      <p>You have {{ totalCount }} tasks left to do</p>
+      <div v-for="task in tasks">
         <TaskDetails :task="task" />
       </div>
     </div>
     <!-- favs list -->
     <div class="task-list" v-if="filter === 'favs'">
-      <p>You have {{ TaskStore.favCount }} tasks left to do</p>
-      <div v-for="task in TaskStore.favs">
+      <p>You have {{ favCount }} tasks left to do</p>
+      <div v-for="task in favs">
         <TaskDetails :task="task" />
       </div>
     </div>
+
+    <button @click="TaskStore.$reset">reset store</button>
   </main>
 </template>
 
@@ -42,8 +44,11 @@ import { ref } from 'vue'
 import { useTaskStore } from './stores/TaskStore'
 import TaskDetails from './components/TaskDetails.vue'
 import TaskForm from './components/TaskForm.vue'
+import { storeToRefs } from 'pinia'
 
 const TaskStore = useTaskStore()
+
+const { tasks, loading, favs, totalCount, favCount } = storeToRefs(TaskStore)
 
 // fetch tasks
 TaskStore.getTasks()
